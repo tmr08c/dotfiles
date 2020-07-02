a56535e7ba12add352a6dc648e9c3a4c1a94a84c
Troy Rosenberg <troy.rosenberg@mckesson.com>
2020-07-02 18:29:07 -0400

    WIP - updating common commands
    * cat --> bat
    * ls --> exa

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,16 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
+# exa overides for ls
+if $(exa &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="exa -F"
+  alias l="exa -l"
+  alias ll="exa -la"
+  alias la="exa -a"
+fi
+
+# `cat`
+if $(type bat &>/dev/null)
+then
+  # alias cat="bat""OneHalfLight"
+  alias cat="bat --theme=\$(darkMode =~ 'dark' && echo default || echo ansi-light)"
+
 fi
