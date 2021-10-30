7872ead09568415f54cc6172fd74e1ce9ec7a21f
Troy Rosenberg <troy.rosenberg@cpowerenergymanagement.com>
2021-10-30 19:31:40 -0400

    (emacs) Convert configuration to be an org file
    - Enable literate programming module in DOOM
    - Move `config.el` to `config.org`
    
      This was mostly copying and pasting, but I also added a few headers,
      moved comments out of elisp and into org notes, and group some
      configuration .
    
    - Add `config.el` to `.gitignore`
    
      This way we keep our  "raw"
      config (`config.org`) checked into version control and do not get our
      "compiled" version (`config.el`).

--- emacs/doom.d.symlink/custom.el
+++
@@ -1,9 +0,0 @@
-;; https://github.com/d12frosted/homebrew-emacs-plus#system-appearance-change"
-(defun my/apply-theme (appearance)
-  "Load theme, taking current system APPEARANCE into consideration."
-  (mapc #'disable-theme custom-enabled-themes)
-  (pcase appearance
-    ('light (load-theme 'doom-one-light t))
-    ('dark (load-theme 'doom-one t))))
-
-(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
