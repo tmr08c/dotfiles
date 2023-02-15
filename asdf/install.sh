6192dce487ecafb5d6f9566f4252f6a97b7f620f
Troy Rosenberg <tmr08c@gmail.com>
2023-02-15 10:28:09 -0500

    (setup) Create `install.sh` script for asdf
    Create an install script for asdf that will add plugins I use and
    install the latest version for each language managed.

---
+++ asdf/install.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+echo "Setting up asdf"
+
+array=(direnv erlang elixir golang julia nodejs rust zig)
+for lang in "${array[@]}"; do
+    echo "Adding plugin for '$lang'"
+    asdf plugin add $lang
+    echo "Installing latest version of '$lang'"
+    asdf install $lang latest
+    asdf global $lang latest
+done
