147a13f5fd54e71c7a913d282b14de4338948aa3
Troy Rosenberg <tmr08c@gmail.com>
2023-02-28 14:54:36 -0500

    (asdf) Add additional languages to install

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,7 +2,7 @@
 
 echo "Setting up asdf"
 
-array=(direnv erlang elixir golang julia nodejs rust zig)
+array=(direnv erlang elixir golang julia nodejs python ruby rust zig)
 for lang in "${array[@]}"; do
     echo "Adding plugin for '$lang'"
     asdf plugin add $lang
