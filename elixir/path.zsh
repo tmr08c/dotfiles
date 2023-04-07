32c4eebbc0cf2660b752f6362e6a50d6f6919f6c
Troy Rosenberg <tmr08c@gmail.com>
2023-04-07 16:38:41 -0400

    (elixir) Remove elixir-ls git submodule
    I should now be relying on the Emacs LSP package to fetch elixir-ls, so
    I should no longer need to manage it myself.

--- elixir/path.zsh
+++
@@ -1,4 +0,0 @@
-# Set up elixir-ls for emacs
-# - https://github.com/elixir-lsp/elixir-ls#building-and-running
-# - https://dev.to/mariomazo/elixir-and-doom-emacs-m29
-export PATH=$PATH:$HOME/.dotfiles/elixir/elixir-ls/release
