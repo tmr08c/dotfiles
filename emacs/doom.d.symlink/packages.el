396e8250cb72be9b239c23b6b0261ae5087068d7
Troy Rosenberg <tmr08c@gmail.com>
2023-04-07 16:40:33 -0400

    (emacs) Install code-compass package

--- emacs/doom.d.symlink/packages.el
+++ emacs/doom.d.symlink/packages.el
@@ -63,3 +63,7 @@
 ;; or an unread email count in my modeline.
 (when (modulep! :email mu4e)
   (package! mu4e-alert :disable t))
+
+
+(package! code-compass
+  :recipe (:host github :repo "ag91/code-compass" :branch "main" :files (:defaults "scripts" "pages")))
