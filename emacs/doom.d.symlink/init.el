b5fe860c8d4a5df51b4b77a14ea8bf5b91d065d8
Troy Rosenberg <troy.rosenberg@blvd.co>
2024-04-15 09:51:58 -0400

    (emacs, javascript) Set up tree-sitter for TS
    - enable tree-sitter modules for web and JS
    - explicitly pull in and set up typescript tree-sitter for TS and TSX

--- emacs/doom.d.symlink/init.el
+++ emacs/doom.d.symlink/init.el
@@ -144,7 +144,7 @@
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
        ;;(java +lsp) ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;; (julia +lsp)             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -174,7 +174,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
+       (web +lsp +tree-sitter)               ; the tubes
        yaml              ; JSON, but readable
        ;; zig                ; C, but simpler
 
