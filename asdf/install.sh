8faf2aa339bfefb0acc14e8d4ea7cea7ead8d014
Troy Rosenberg <tmr08c@gmail.com>
2025-04-30 14:20:15 -0400

    (asdf) Update asdf install script
    - set up zsh compleitions
    - add java

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,13 +2,16 @@
 
 echo "Setting up asdf"
 
-array=(direnv erlang elixir golang julia nodejs python ruby rust typos zig)
+mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
+
+array=(direnv erlang elixir golang java julia nodejs python ruby rust typos zig)
 for lang in "${array[@]}"; do
     echo "Adding plugin for '$lang'"
     asdf plugin add $lang
     echo "Installing latest version of '$lang'"
     asdf install $lang latest
-    asdf global $lang latest
+    asdf set --home $lang latest
 done
 
 exit 0;
